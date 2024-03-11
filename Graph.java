2d7a4e30924d1be823f885f99e21443014f1e5ae
Garaton <garettbearr101@gmail.com>
2024-03-11 14:47:10 -0400

    INITIAL COMMIT
    Overview :
    Set up a basic overview of what I want to include
    as functions for Graph.java and Main.java.

---
+++ Graph.java
@@ -0,0 +1,17 @@
+import java.math.*;
+
+/**
+ * @author      Garett Rogers
+ * @version     0
+ */
+// add a @since
+public class Graph {
+    // PIVS which includes the graph 2d array
+
+    // constructor method, takes input for graph dimension and scale
+
+    // function parcer, takes string input and turns it into a recognised function
+    // this function will also fill out the graph array
+
+    // function that displays the graph in the array
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @author      Garett Rogers
+ * @version     0
+ */
+// add a @since when ready
+public class Main {
+    /**
+     * Main method takes input and uses Graph.java to make and display graphs
+    */
+    public void main(){
+        // setting up variables
+        Scanner input = new Scanner(System.in);
+
+        // main input code
+
+        input.close();
+    }
+}
